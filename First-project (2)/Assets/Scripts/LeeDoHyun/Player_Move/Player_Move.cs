a141da811bb1be881fe57fed0fd9c974d60a5506
EDO-MAN <73830965+EDO-MAN@users.noreply.github.com>
2021-04-13 23:44:58 +0900

    턴제 버려 -> 플랫폼 진행
    플레이어 움직임o(애니x)

---
+++ First-project (2)/Assets/Scripts/LeeDoHyun/Player_Move/Player_Move.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Move : MonoBehaviour
+{
+    public float maxSpeed;
+    public float jumpPower;
+
+    Rigidbody2D rigid;
+    SpriteRenderer sprite;
+    Animator anim;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        //Jump
+        if (Input.GetButtonDown("Jump") /*&& !anim.GetBool("isJumping")*/)
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        }
+
+        //Stop speed
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+        }
+        //DirectionX 
+        if (Input.GetButton("Horizontal"))
+        {
+            sprite.flipX = Input.GetAxisRaw("Horizontal") == -1;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //Move By Key Control
+        float h = Input.GetAxisRaw("Horizontal");
+
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        if (rigid.velocity.x > maxSpeed)//Right Max Speed
+        {
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        }
+        else if (rigid.velocity.x < maxSpeed * (-1))//Left Max Speed
+        {
+            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+        }
+    }
+}
